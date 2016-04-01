61190e4e46e434b8b6932e212d433c0c4393b948
aman-tugnawat <aman.tugnawat@gmail.com>
2016-04-02 00:52:21 +0530

    initial commit php server

---
+++ Server-php/dbConnect.php
@@ -0,0 +1,8 @@
+<?php
+ define('HOST','mysql.hostinger.in');
+ define('USER','u133218288_db');
+ define('PASS','qwerty123456');
+ define('DB','u133218288_db');
+ 
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+ ?>
